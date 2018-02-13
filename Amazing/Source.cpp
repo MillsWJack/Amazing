dd5ac4b6c107b67e05c5a4359712fd8ea15c02dc
MillsWJack <millswjack@googlemail.com>
2018-02-13 15:48:58 +0000

    Psuedo Code Added

---
+++ Amazing/Source.cpp
@@ -0,0 +1,25 @@
+//Amazing
+/*
+	This program will print out a different maze every time it is run
+	and guarantees only one path through. You can choose the dimensions
+	of the maze. i.e. the number of squares wide and long.
+*/
+
+/*
+	PSEUDO
+	Make the initial cell the current cell and mark it as visited
+	While there are unvisited cells
+		If the current cell has any neighbours which have not been visited
+			Choose randomly one of the unvisited neighbours
+			Push the current cell to the stack
+			Remove the wall between the current cell and the chosen cell
+			Make the chosen cell the current cell and mark it as visited
+		Else if stack is not empty
+			Pop a cell from the stack
+			Make it the current cell
+*/
+
+int main()
+{
+	return 0;
+}
